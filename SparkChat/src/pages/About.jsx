0429a9a30af48adacd930a46d74f5682c3156d08
umesh-22 <uyadhav7@gmail.com>
2024-10-28 13:43:28 +0530

    "Refactored About page: reformatted text, added line breaks, and updated font sizes and styles."

--- SparkChat/src/pages/About.jsx
+++ SparkChat/src/pages/About.jsx
@@ -13,20 +13,24 @@ const About = () => {
           onClick={handleBackButton}
           className="text-5xl text-white/70 cursor-pointer pr-3 hover:text-blue-500"
         />
-        <h1 className="text-3xl lg:text-5xl text-center text-white/70 ">About</h1>
+        <h1 className="text-3xl lg:text-5xl text-center text-white/70 ">
+          About
+        </h1>
       </div>
       <div className="flex flex-col min-h-screen justify-normal items-center  space-y-2 px-5">
         <p className="text-xl">
-          Welcome to <strong className="text-blue-500">SparkChat</strong> – a seamless, modern chat
-          application designed to connect you with friends, family, and
-          colleagues.
+          Welcome to <strong className="text-blue-500">SparkChat</strong> – a
+          seamless, modern chat application designed to connect you with
+          friends, family, and colleagues.
         </p>
         <p className="text-lg">
           Built with user experience and real-time communication in mind,
           SparkChat ensures that every message is just a click away.
         </p>
         <div className="">
-          <h2 className="text-2xl font-semibold mb-1 text-blue-500 ">Features</h2>
+          <h2 className="text-xl md:text-2xl font-semibold mb-1 text-blue-500 ">
+            Features
+          </h2>
           <ul className="list-disc list-inside space-y-2 ml-5">
             <li>
               <strong>Real-Time Messaging</strong>: Instantly send and receive
@@ -42,10 +46,11 @@ const About = () => {
               of emojis to make your conversations more engaging.
             </li>
           </ul>
-         
         </div>
         <div className="">
-        <h2 className="text-2xl font-semibold mb-1 text-blue-500">Upcoming Features</h2>
+          <h2 className="text-xl md:text-2xl font-semibold mb-1 text-blue-500">
+            Upcoming Features
+          </h2>
           <p>
             <strong>Channel Feature</strong>: Soon, SparkChat will introduce
             channels – a powerful way to bring groups together for organized,
@@ -53,7 +58,9 @@ const About = () => {
             multiple people at once, making it easy to collaborate and stay
             updated.
           </p>
-          <h2 className="text-2xl font-semibold mb-1 text-blue-500">Security Commitment</h2>
+          <h2 className="text-xl md:text-2xl font-semibold mb-1 text-blue-500">
+            Security Commitment
+          </h2>
           <p>
             We prioritize user privacy and security. In an upcoming update, we
             will implement{" "}
@@ -63,13 +70,18 @@ const About = () => {
             . This will ensure that your messages remain private and accessible
             only to you and the intended recipient.
           </p>
-          <h2 className="text-2xl font-semibold mb-1 text-blue-500">
+          <h2 className="text-xl md:text-2xl font-semibold mb-1 text-blue-500">
             Join Us on the Journey!
           </h2>
           <p>
             We’re constantly evolving SparkChat to offer a more intuitive,
             feature-rich chat experience. Keep an eye out for exciting updates,
             and thank you for being a part of SparkChat’s journey!
+          </p>
+
+          <p className="">
+            To Contact us{" "}
+            <span className="text-blue-500">sparkchat2024@gmail.com</span>
           </p>
         </div>
       </div>
